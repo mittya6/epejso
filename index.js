5bdca64010bf512e8b778bc8506f55cec07825d9
mittya6 <mittya6@gmail.com>
2021-04-06 19:26:29 +0900

    init

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-const fs = require("fs").promises;
+const fs = require("fs");
 const ejs = require("ejs");
 const marked = require("marked");
 const path = require('path');
 const { Command } = require('commander');
-//const Prism = require("prismjs");
+
+const renderer = new marked.Renderer();
+renderer.image = function(href, title, text) {   
+  return `<div uk-lightbox>
+              <a class="uk-button uk-button-default" href="${href}"><img src="${href}" uk-img></a>
+          </div>`;
+}
 
 /*
 marked.setOptions({
@@ -31,37 +37,36 @@ const program = new Command();
 program.option("-f, --filepath <value>", "test option").parse(process.argv);
 
 program.on('--help', () => {
-    console.log()
-    console.log('For more information, see')
-    console.log('https://github.com/hktysk/resv')
-    console.log()
+  console.log()
+  console.log('For more information, see')
+  console.log('https://github.com/hktysk/resv')
+  console.log()
 });
 
-let message;
-(async () => {
-    message = await fs.readFile(program.opts().filepath, 'utf-8');
+const message = fs.readFileSync(program.opts().filepath, 'utf-8');
+ejs.renderFile('./index.ejs', {
 
-    ejs.renderFile('./index.ejs', {
+  //temp.ejsに渡す値
+  prismjs: fs.readFileSync('node_modules/prismjs/prism.js','utf-8'),
+  prismtoolbarminjs: fs.readFileSync('node_modules/prismjs/plugins/toolbar/prism-toolbar.min.js','utf-8'),
+  prismcopytoclipboardminjs: fs.readFileSync('node_modules/prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard.min.js','utf-8'),
+  uikitminjs: fs.readFileSync('node_modules/uikit/dist/js/uikit.min.js','utf-8'),
+  inviewminjs: fs.readFileSync('node_modules/in-view/dist/in-view.min.js','utf-8'),
+  prismtoolbarcss: fs.readFileSync('node_modules/prismjs/plugins/toolbar/prism-toolbar.css','utf-8'),
+  uikitmincss: fs.readFileSync('node_modules/uikit/dist/css/uikit.my.uikit.min.css','utf-8'),
+  prismcss: fs.readFileSync('node_modules/prismjs/themes/prism.css','utf-8'),  
+  content: marked(message, { renderer: renderer })
 
-        //temp.ejsに渡す値
-        content: marked(message)
-
-    }, function (err, html) {
-      if(err){
-        console.log(err.stack);
-        return;
-      }
-        // 出力ファイル名
-        const file = path.dirname(program.opts().filepath) + '/index.html';
-        // テキストファイルに書き込む
-        fs.writeFile(file, html, 'utf8', (err) => {
-            if (err) {
-                console.log(err);
-            } else {
-                console.log('保存完了');
-            }
-        });
-    });
-})();
+}, function (err, html) {
+  if (err) {
+    console.log(err.stack);
+    return;
+  }
+  // 出力ファイル名
+  const file = path.dirname(program.opts().filepath) + '/index.html';
+  // テキストファイルに書き込む
+  fs.writeFileSync(file, html, 'utf8');
+  console.log("完了")
+});
 
 
