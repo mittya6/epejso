d590f82a4b6ad83c05ae2690b9b7d1883f23858e
Yasuda <mittya6@gmail.com>
2021-04-21 23:31:19 +0900

    init

--- renderer/defaultRenderer.js
+++ renderer/defaultRenderer.js
@@ -4,16 +4,16 @@ const fs = require("fs");
 
 module.exports = (mddir) => {
 
-    const renderer = new marked.Renderer();
+  const renderer = new marked.Renderer();
 
-    renderer.parseAsDataURL = (file) => {
-        const base64ed = fs.readFileSync(file, { encoding: "base64" });
-        return `data:image/${path.extname(file).replace('.', '')};base64,${base64ed}`;
-    }
+  renderer.parseAsDataURL = (file) => {
+    const base64ed = fs.readFileSync(file, { encoding: "base64" });
+    return `data:image/${path.extname(file).replace('.', '')};base64,${base64ed}`;
+  }
 
-    renderer.image = function (href, title, text) {
-        const dataURI = this.parseAsDataURL(path.join(mddir, href));
-        return `<div class="uk-width-1-4 uk-text-center" uk-lightbox>
+  renderer.image = function (href, title, text) {
+    const dataURI = this.parseAsDataURL(path.join(mddir, href));
+    return `<div class="uk-width-1-3 uk-text-center" uk-lightbox>
             <div class="uk-inline-clip uk-transition-toggle" tabindex="0">
               <a class="uk-button uk-button-default" href="${dataURI}"  data-caption="${text}" data-type="image">
                 <img src="${dataURI}" uk-img>
@@ -23,13 +23,14 @@ module.exports = (mddir) => {
               </a>
             </div>
           </div>`;
-    }
-    renderer.table = function (header, body) {
-        return `<table class="uk-table uk-table-divider">'
+  }
+  renderer.table = function (header, body) {
+    return `<div class="uk-card uk-card-default uk-card-body uk-width-2-3">
+          <table class="uk-table uk-table-divider">'
             <thead>${header}</thead>
             <tbody>${body}</tbody>
-          </table>`;
-    };
+          </table></div>`;
+  };
 
-    return renderer;
+  return renderer;
 }
