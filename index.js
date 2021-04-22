10d7bb7b381d2aca4efa146edc900783b0495c06
Yasuda <mittya6@gmail.com>
2021-04-23 01:00:58 +0900

    ini

--- index.js
+++ index.js
@@ -14,49 +14,60 @@ program
   .option("-o, --outputdir <value>", "output directory")
   .option("-d, --tarDir <value>", "target directory")
   .parse(process.argv);
-if (!program.opts().filepath &&  !program.opts().tarDir){
-  throw new Error("-f or -d required")
+if (!program.opts().filepath && !program.opts().tarDir) {
+  throw new Error("-f or -d required");
 }
-const ejspath = (program.opts().ejspath)?program.opts().ejspath:'./template/default.ejs';
-const rendererpath = (program.opts().rendererpath)?program.opts().rendererpath:'./renderer/defaultRenderer.js';
-const renderer = (require(rendererpath))(path.dirname(program.opts().filepath));
-const outputdir = (program.opts().outputdir)?program.opts().outputdir:'../';
+const ejspath = (program.opts().ejspath) ? program.opts().ejspath : './template/default.ejs';
+const rendererpath = (program.opts().rendererpath) ? program.opts().rendererpath : './renderer/defaultRenderer.js';
 
-let tarMd = path.join(outputdir, '**/*.md');
-glob(tarMd,{'ignore':['**/node_modules/**/*','**/README.md']}, (err, files) => {
-  files.forEach(file => {
-      console.log(file);
+const outputdir = (program.opts().outputdir) ? program.opts().outputdir : '../';
+
+const mdfiles = [];
+
+if (program.opts().filepath) {
+  mdfiles.push(program.opts().filepath);
+
+} else {
+  let tarMd = path.join(outputdir, '**/*.md');
+  glob(path.resolve(tarMd), { 'ignore': ['**/node_modules/**', '**/README.md'] }, (err, files) => {
+    files.forEach(file => {
+      mdfiles.push(file);
+    });
   });
-});
+}
 
+mdfiles.forEach((mdfile) => { makehtml(mdfile); });
 
-/** parse marked content start */
-const markedContents = marked(fs.readFileSync(program.opts().filepath, 'utf-8'), { renderer: renderer });
-/** parse marked content end */
+function makehtml(tarMdfile) {
+  /** parse marked content start */
+  const renderer = (require(rendererpath))(path.dirname(program.opts().filepath));
+  const markedContents = marked(fs.readFileSync(program.opts().filepath, 'utf-8'), { renderer: renderer });
+  /** parse marked content end */
 
-ejs.renderFile(ejspath, {
+  ejs.renderFile(ejspath, {
 
-  title: markedContents['meta']['Title'],
-  createDate: markedContents['meta']['CreatedDate'],
-  updateDate: markedContents['meta']['UpdatedDate'],
-  //temp.ejsに渡す値
-  prismjs: fs.readFileSync('node_modules/prismjs/prism.js', 'utf-8'),
-  prismtoolbarminjs: fs.readFileSync('node_modules/prismjs/plugins/toolbar/prism-toolbar.min.js', 'utf-8'),
-  prismcopytoclipboardminjs: fs.readFileSync('node_modules/prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard.min.js', 'utf-8'),
-  uikitminjs: fs.readFileSync('node_modules/uikit/dist/js/uikit.min.js', 'utf-8'),
-  prismtoolbarcss: fs.readFileSync('node_modules/prismjs/plugins/toolbar/prism-toolbar.css', 'utf-8'),
-  uikitmincss: fs.readFileSync('custom/my.uikit.css', 'utf-8'),
-  prismcss: fs.readFileSync('node_modules/prism-themes/themes/prism-material-light.css', 'utf-8'),
-  content: markedContents['html']
+    title: markedContents['meta']['Title'],
+    createDate: markedContents['meta']['CreatedDate'],
+    updateDate: markedContents['meta']['UpdatedDate'],
+    //temp.ejsに渡す値
+    prismjs: fs.readFileSync('node_modules/prismjs/prism.js', 'utf-8'),
+    prismtoolbarminjs: fs.readFileSync('node_modules/prismjs/plugins/toolbar/prism-toolbar.min.js', 'utf-8'),
+    prismcopytoclipboardminjs: fs.readFileSync('node_modules/prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard.min.js', 'utf-8'),
+    uikitminjs: fs.readFileSync('node_modules/uikit/dist/js/uikit.min.js', 'utf-8'),
+    prismtoolbarcss: fs.readFileSync('node_modules/prismjs/plugins/toolbar/prism-toolbar.css', 'utf-8'),
+    uikitmincss: fs.readFileSync('custom/my.uikit.css', 'utf-8'),
+    prismcss: fs.readFileSync('node_modules/prism-themes/themes/prism-material-light.css', 'utf-8'),
+    content: markedContents['html']
 
-}, function (err, html) {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  // 出力ファイル名
-  const file = path.join(outputdir, markedContents['meta']['Title'] + '.html');
-  // テキストファイルに書き込む
-  fs.writeFileSync(file, html, 'utf8');
-  console.log("完了")
-});
+  }, function (err, html) {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    // 出力ファイル名
+    const file = path.join(outputdir, markedContents['meta']['Title'] + '.html');
+    // テキストファイルに書き込む
+    fs.writeFileSync(file, html, 'utf8');
+    console.log("完了")
+  });
+}
