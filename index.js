a28cf6c121a31b3eda811fce9b2d34a2ceefecbf
mittya6 <mittya6@gmail.com>
2021-04-24 11:53:45 +0900

    init

--- index.js
+++ index.js
@@ -4,21 +4,19 @@ const marked = require("meta-marked");
 const path = require('path');
 const { Command } = require('commander');
 const glob = require('glob');
-
+const config = require('config');
 
 const program = new Command();
 program
   .option("-f, --filepath <value>", "markdown file path")
-  .option("-e, --ejspath <value>", "ejs template file path")
-  .option("-r, --rendererpath <value>", "renderer file path")
   .option("-o, --outputdir <value>", "output directory")
   .option("-d, --tarDir <value>", "target directory")
   .parse(process.argv);
 if (!program.opts().filepath && !program.opts().tarDir) {
   throw new Error("-f or -d required");
 }
-const ejspath = (program.opts().ejspath) ? program.opts().ejspath : './template/default.ejs';
-const rendererpath = (program.opts().rendererpath) ? program.opts().rendererpath : './renderer/defaultRenderer.js';
+const ejspath =  config.get('ejs.template');
+const rendererpath = config.get("marked.renderer");
 
 const outputdir = (program.opts().outputdir) ? program.opts().outputdir : '../';
 
@@ -36,7 +34,7 @@ if (program.opts().filepath) {
   });
 }
 
-function makehtml(tarMdfile) {
+const makehtml = (tarMdfile) => {
   
   /** parse marked content start */
   const renderer = (require(rendererpath))(path.dirname(tarMdfile));
@@ -48,6 +46,7 @@ function makehtml(tarMdfile) {
     title: markedContents['meta']['Title'],
     createDate: markedContents['meta']['CreatedDate'],
     updateDate: markedContents['meta']['UpdatedDate'],
+    content: markedContents['html'],
     //temp.ejsに渡す値
     prismjs: fs.readFileSync('node_modules/prismjs/prism.js', 'utf-8'),
     prismtoolbarminjs: fs.readFileSync('node_modules/prismjs/plugins/toolbar/prism-toolbar.min.js', 'utf-8'),
@@ -55,8 +54,7 @@ function makehtml(tarMdfile) {
     uikitminjs: fs.readFileSync('node_modules/uikit/dist/js/uikit.min.js', 'utf-8'),
     prismtoolbarcss: fs.readFileSync('node_modules/prismjs/plugins/toolbar/prism-toolbar.css', 'utf-8'),
     uikitmincss: fs.readFileSync('custom/my.uikit.css', 'utf-8'),
-    prismcss: fs.readFileSync('node_modules/prism-themes/themes/prism-material-light.css', 'utf-8'),
-    content: markedContents['html']
+    prismcss: fs.readFileSync('node_modules/prism-themes/themes/prism-material-light.css', 'utf-8')
 
   }, function (err, html) {
     if (err) {
