2895566be07f0a71ab696e7462e456aeb5ceb541
Yasuda <mittya6@gmail.com>
2021-04-25 17:34:52 +0900

    init

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ program
   .option("-d, --tarDir <value>", "target directory")
   .parse(process.argv);
 
-const ejspath =  config.get('ejs.template');
+const ejspath = config.get('ejs.template');
 const rendererpath = config.get("ejs.marked.renderer");
 
 const outputdir = (program.opts().outputdir) ? program.opts().outputdir : '../';
@@ -24,7 +24,7 @@ if (program.opts().filepath) {
 
 } else {
   let tarMd = path.join(outputdir, '**/*.md');
-  glob(path.resolve(tarMd), { 'ignore': ['**/md2runbook/**','**/node_modules/**', '**/README.md'] }, (err, files) => {
+  glob(path.resolve(tarMd), { 'ignore': ['**/md2runbook/**', '**/node_modules/**', '**/README.md'] }, (err, files) => {
     files.forEach(mdfile => {
       console.log(`marked ${mdfile}`);
       makehtml(mdfile);
@@ -32,55 +32,47 @@ if (program.opts().filepath) {
   });
 }
 
-console.log(config);
 
-if(config.has('ejs.include.file')){
+const includeFiles = {};
+if (config.has('ejs.include.file')) {
   let obj = config.get('ejs.include.file');
   Object.keys(obj).forEach(function (key) {
-    console.log(key);
-    console.log(obj[key]);
+    includeFiles[key] = fs.readFileSync(obj[key], 'utf-8');
   });
 }
 
 
 const makehtml = (tarMdfile) => {
-  
   /** parse marked content start */
   const renderer = (require(rendererpath))(path.dirname(tarMdfile));
   const markedContents = marked(fs.readFileSync(tarMdfile, 'utf-8'), { renderer: renderer });
-  if(!markedContents['meta']){
+  if (!markedContents['meta']) {
     markedContents['meta'] = [];
   }
-  if(!markedContents['meta']['Title']){
+  if (!markedContents['meta']['Title']) {
     markedContents['meta']['Title'] = "no title";
   }
   /** parse marked content end */
 
-  ejs.renderFile(ejspath, {
-
+  let ejsParams = {
     title: markedContents['meta']['Title'],
     createDate: markedContents['meta']['CreatedDate'],
     updateDate: markedContents['meta']['UpdatedDate'],
-    content: markedContents['html'],
-    //temp.ejsに渡す値
-    prismjs: fs.readFileSync('node_modules/prismjs/prism.js', 'utf-8'),
-    prismtoolbarminjs: fs.readFileSync('node_modules/prismjs/plugins/toolbar/prism-toolbar.min.js', 'utf-8'),
-    prismcopytoclipboardminjs: fs.readFileSync('node_modules/prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard.min.js', 'utf-8'),
-    uikitminjs: fs.readFileSync('node_modules/uikit/dist/js/uikit.min.js', 'utf-8'),
-    prismtoolbarcss: fs.readFileSync('node_modules/prismjs/plugins/toolbar/prism-toolbar.css', 'utf-8'),
-    uikitmincss: fs.readFileSync('custom/my.uikit.css', 'utf-8'),
-    prismcss: fs.readFileSync('node_modules/prism-themes/themes/prism-material-light.css', 'utf-8')
+  }
+  ejsParams[config.get("ejs.marked.ejsKey")] = markedContents['html'];
 
-  }, function (err, html) {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    // 出力ファイル名
-    const file = path.join(outputdir, markedContents['meta']['Title'] + '.html');
-    // テキストファイルに書き込む
-    
-    fs.writeFileSync(file, html, 'utf8');
-    console.log(`output ${file}`);
-  });
+  ejs.renderFile(ejspath,
+    Object.assign(ejsParams, includeFiles)
+    , function (err, html) {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      // 出力ファイル名
+      const file = path.join(outputdir, markedContents['meta']['Title'] + '.html');
+      // テキストファイルに書き込む
+
+      fs.writeFileSync(file, html, 'utf8');
+      console.log(`output ${file}`);
+    });
 }
