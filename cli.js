1d0bee8a190f5fba78eb7a041070a818e7cc291a
mittya6 <mittya6@gmail.com>
2021-05-17 07:31:13 +0900

    init

--- cli.js
+++ cli.js
@@ -18,8 +18,8 @@ program
   .parse(process.argv);
 // parse Start argument end
 
-const ejspath = path.join(__dirname__ ,config.get('ejs.template'));
-const rendererpath = path.join(__dirname__ ,config.get("ejs.marked.renderer"));
+const ejspath = path.join(__dirname ,config.get('ejs.template'));
+const rendererpath = path.join(__dirname ,config.get("ejs.marked.renderer"));
 
 console.log("start md2runbook");
 
