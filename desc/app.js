6c9170a5d601acdebed4141f76fbc263260ba131
mittya6 <mittya6@gmail.com>
2023-03-16 22:37:23 +0900

    init

--- desc/app.js
+++ desc/app.js
@@ -1,13 +1,11 @@
+#!/usr/bin/env node
 import ejs from 'ejs';
 import * as path from 'path';
 import { fileURLToPath } from 'url';
-console.log("Hello! Node.js × TypeScript");
 const __filename = path.join(fileURLToPath(import.meta.url), '../');
 const __dirname = path.dirname(__filename);
-console.log(__dirname);
 const ejspath = path.join(__dirname, './template/index.ejs');
 ejs.renderFile(ejspath, {
-    //temp.ejsに渡す値
     testText: "test",
     title: "hoge"
 }, function (err, html) {
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,8 @@
+#!/usr/bin/env node
+
 import ejs from 'ejs'
 import * as path from 'path'
 import { fileURLToPath } from 'url';
-
-
-console.log("Hello! Node.js × TypeScript")
-
 
 const __filename = path.join(fileURLToPath(import.meta.url),'../')
 const __dirname = path.dirname(__filename)
